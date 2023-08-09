f02c4bc753f150571fc412fcff5fe4ede3eb0136
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-08-09 11:46:06 -0400

    the actual code stuff

---
+++ pixy.js
@@ -0,0 +1,55 @@
+class GLPixy {
+  constructor(off, size, img, pixSize, fbo, rdr = window, canvas = _renderer, gl = drawingContext) {
+    this.gl = gl;
+    this.off = off;
+    this.size = size;
+    this.res = img.res;
+    this.img = img;
+    this.pixSize = pixSize;
+    this.fbo = fbo;
+    this.loadPixels();
+    this.rdr = rdr;
+    this.canvas = canvas;
+  }
+
+  loadPixels() {
+    this.fbo.linkTexture2D(this.gl.COLOR_ATTACHMENT0, this.img.bufType, this.img.tex, this.img.level);
+    this.fbo.readPixels(0, 0, this.img.res[0], this.img.res[1], this.img.srcFormat, this.img.srcType, this.img.pixel);
+  }
+
+  updatePixels() {    
+    this.img.subData(this.img.level, 0, 0, this.img.res[0], this.img.res[1], this.img.srcFormat, this.img.srcType, this.img.pixel);
+  }
+
+  setInterpolation(min, mag) {
+    img.paramI(this.gl.TEXTURE_MAG_FILTER, min);
+    img.paramI(this.gl.TEXTURE_MIN_FILTER, mag);
+  }
+
+  getPixPos(loc) {
+    return (loc[0] + this.img.res[0] * loc[1]) * this.pixSize;
+  }
+
+  getPixel(loc) {
+    const pixPos = this.getPixPos(loc);
+    const pix = new Array(this.pixSize);
+    for (const i in pix)
+      pix[i] = this.img.pixel[pixPos + i];
+    return pix;
+  }
+
+  setPixel(loc, col) {
+    const pixPos = this.getPixPos(loc);
+    for (let i = 0; i < this.pixSize; i++)
+      this.img.pixel[pixPos + i] = col[i];
+  }
+
+  clear() {
+    this.gl.clear(gl.COLOR_BUFFER_BIT);
+  }
+
+  display(shad) {
+    pixy.img.setUniform(shad._glProgram, "uSampler");
+    this.rdr.rect(this.off[0], this.off[1], this.size[0], this.size[1]);
+  }
+}
